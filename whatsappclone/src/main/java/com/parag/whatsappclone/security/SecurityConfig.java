d1aff962f0f4e24baf911fa39089e935d190d40f
Parag Goyal <1997parag.goyal@gmail.com>
2025-05-24 17:47:05 +0530

    add security config and initial config related to database and authentication

---
+++ whatsappclone/src/main/java/com/parag/whatsappclone/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.parag.whatsappclone.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                            req.requestMatchers("/auth/**",
+                                            "/v2/api-docs",
+                                            "/v3/api-docs",
+                                            "/v3/api-docs/**",
+                                            "/swagger-resources",
+                                            "/swagger-resources/**",
+                                            "/configuration/ui",
+                                            "/configuration/security",
+                                            "/swagger-ui/**",
+                                            "/webjars/**",
+                                            "/swagger-ui.html",
+                                            "/ws/**")
+                                .permitAll()
+                                    .anyRequest()
+                                    .authenticated()
+                        );
+
+        return http.build();
+    }
+}
